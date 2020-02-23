2ac0cf1dad601abe7b3f5d03944a578d1e537a01
Anupam Rai <anupamit011@gmail.com>
2020-02-24 01:35:51 +0530

    JavaScript file for contact list

---
+++ contacts.js
@@ -0,0 +1,25 @@
+const contacts = [
+  {
+    name: "Shalu Rai",
+    imgURL:
+      "https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg",
+    phone: "+91-825432876",
+    email: "shaluraics@gmail.com"
+  },
+  {
+    name: "Ritik Singh",
+    imgURL:
+      "https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg",
+    phone: "+91-9845363643",
+    email: "ritiksinghit036@gmail.com"
+  },
+  {
+    name: "Maanu Rai",
+    imgURL:
+      "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
+    phone: "+91-8202869533",
+    email: "manurai800@gmail.com"
+  }
+];
+
+export default contacts;
